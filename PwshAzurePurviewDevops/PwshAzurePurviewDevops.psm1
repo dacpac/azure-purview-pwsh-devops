c413af5968e04fd014718526389975aace35f6d3
Craig Ottley-Thistlethwaite <craig.ottley-thistlethwaite@sql.limited>
2021-01-07 17:06:16 +0000

    Changed Foreach-Object to foreach

--- PwshAzurePurviewDevops/PwshAzurePurviewDevops.psm1
+++ PwshAzurePurviewDevops/PwshAzurePurviewDevops.psm1
@@ -1,8 +1,8 @@
 <# Get Public and Private function definition files. #>
-$Public = @( Get-ChildItem -Path $PSScriptRoot/Public/*.ps1 -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot/Private/*.ps1 -ErrorAction SilentlyContinue )
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
-ForEach-Object ($import in @($Private + $Public)) {
+foreach ($import in @($Private + $Public)) {
     try {
         . $import.FullName
     }
